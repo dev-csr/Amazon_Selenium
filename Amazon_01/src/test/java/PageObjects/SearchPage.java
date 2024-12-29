2b1b087c82c0edb9847c7e72d06eb947abd1cc70
dev-csr <srikanthchilukuri01@gmail.com>
2024-12-29 16:22:56 +0530

    Second commit

--- Amazon_01/src/test/java/PageObjects/SearchPage.java
+++ Amazon_01/src/test/java/PageObjects/SearchPage.java
@@ -1,5 +1,31 @@
 package PageObjects;
 
-public class SearchPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+import testBase.BasePage;
+
+public class SearchPage extends BasePage {
+
+	public SearchPage(WebDriver driver) {
+		super(driver);
+	}
+	@FindBy(xpath="//input[@id='twotabsearchtextbox']") WebElement searchAmazon;
+	@FindBy(xpath="//input[@id='nav-search-submit-button']") WebElement go;
+	@FindBy(xpath="//div[@id='88b69834-1258-4911-bb65-4b517447bc42']//div[@class='a-section a-spacing-small a-spacing-top-small puis-padding-right-small']//span[1]//a[1]") WebElement iphone;
+	@FindBy(xpath="//input[@id='add-to-cart-button']") WebElement addToCart;
+	
+	public void search(String product) {
+		searchAmazon.sendKeys(product);
+	}
+	public void submit() {
+		go.submit();
+	}
+	public void clickProduct() {
+		iphone.click();
+	}
+	public void addToCart() {
+		addToCart.click();
+	}
 }
---
+++ Amazon_01/src/test/java/testcases/T002_searchproduct.java
@@ -0,0 +1,25 @@
+package testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import PageObjects.SearchPage;
+import testBase.BaseClass;
+
+public class T002_searchproduct extends BaseClass {
+	@Test(groups={"Sanity","Master"})
+	public void signin() {
+		logger.info("****** Starting T002_searchproduct *********");
+		try {
+			SearchPage sp = new SearchPage(driver);
+			sp.search(p.getProperty("searchProductName"));
+			sp.submit();
+			sp.clickProduct();
+			sp.addToCart();
+		}
+		catch(Exception e) {
+			Assert.fail();
+		}
+		logger.info("******** End of T002_searchproduct *******");
+	}
+}
